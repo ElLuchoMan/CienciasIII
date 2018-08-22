a78ef31cd3f69f6c9771bfed686ae12431b0a4db
Bryan Luis <luchoman96@gmail.com>
2018-08-22 08:02:01 -0500

    Add files via upload

---
+++ Arbol2.0/README.md
@@ -0,0 +1,7 @@
+# CienciasIII
+- Jeisson Rincón - 20152020087
+- Bryan Luis     - 20142020136
+- David Gómez    - 20141020139
+
+Carpeta correspondiente al código de de implementación de arboles para el desarrollo de operaciones en postfijo con el uso de archivos.
+
---
+++ Arbol2.0/arbols_expresiones.py
@@ -0,0 +1,59 @@
+import os
+from pila import *
+from arbol import *
+
+def convertir(lista, pila):
+    if lista != []:
+        if lista[0] in "+-*/=":
+            nodo_der = pila.desapilar()
+            nodo_izq = pila.desapilar()
+            pila.apilar(Nodo(lista[0],nodo_izq,nodo_der))
+        else:
+            pila.apilar(Nodo(lista[0]))
+        return convertir(lista[1:],pila)
+            
+diccionario = dict()
+
+def evaluar(arbol):
+    if arbol.valor == "+":
+        return evaluar(arbol.izq) + evaluar(arbol.der)
+    if arbol.valor == "-":
+        return evaluar(arbol.izq) - evaluar(arbol.der)
+    if arbol.valor == "/":
+        return evaluar(arbol.izq) / evaluar(arbol.der)
+    if arbol.valor == "*":
+        return evaluar(arbol.izq) * evaluar(arbol.der)
+    if arbol.valor == "=":
+        diccionario[arbol.der.valor]=evaluar(arbol.izq)
+        return str(arbol.der.valor)+" = "+str(evaluar(arbol.izq))
+    else:
+        diccionario.get(arbol.valor,default=None)
+        
+        
+    #Debo poner los casos donde aparezcan letras
+    return int(arbol.valor)
+
+    
+
+    
+
+
+f = open ('expresiones.in.txt','r')
+op = f.read().split()
+print(op)
+f.close()
+
+pila = Pila()
+
+convertir(op, pila)
+t = open ('expresiones.out.txt','w')
+
+while (pila.es_vacia() != True):   
+    t.write('resultado: '+ str(evaluar(pila.desapilar())) + os.linesep)
+  #print(pila.es_vacia())
+    
+t.close()
+print(diccionario.items())
+
+
+
